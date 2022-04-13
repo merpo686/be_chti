6f6db435ae291ae23c5e8321e1d5e65ba519887e
Merlin Poitou <poitou@insa-toulouse.fr>
2022-04-13 09:17:44 +0200

    dft en cours de test

--- PjtKEIL_StepDFT/Src/principal.c
+++ PjtKEIL_StepDFT/Src/principal.c
@@ -2,7 +2,7 @@
 
 #include "DriverJeuLaser.h"
 
-extern int DFT_ModuleAuCarre( short int * Signal64ech, char k);
+extern int DFT_ModuleAuCarre( short int * Signal64ech, short int k);
 extern short int LeSignal;
 int main(void)
 {
@@ -20,11 +20,12 @@ CLOCK_Configure();
 
 //============================================================================	
 	
-	
-		char k=1;
-		
-		DFT_ModuleAuCarre(&LeSignal,k);
-		
+		int tab[64];
+		//tab	= malloc(64 * sizeof(int));
+		//int tab[64]={0}; 
+		for (short int i=0;i<64;i++){
+			tab[i]=DFT_ModuleAuCarre(&LeSignal,i);
+		}
 
 }
 
