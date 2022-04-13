21f187320ff8bfabc5539ccca8bc288a2e7704a1
Merlin Poitou <poitou@insa-toulouse.fr>
2022-04-13 10:38:38 +0200

    dma ok, test en réel qui fonctionne peu importe la freq

---
+++ PjtKEIL_DFT_Signal_Reel/Src/principal.c
@@ -0,0 +1,45 @@
+
+
+#include "DriverJeuLaser.h"
+
+extern int DFT_ModuleAuCarre( short int * Signal64ech, short int k);
+int tab[64];
+short int signal[64];
+
+void callback_systick(){
+	Start_DMA1(64);
+	Wait_On_End_Of_DMA1();
+	Stop_DMA1;
+	for (short int i=0;i<64;i++){
+			tab[i]=DFT_ModuleAuCarre(signal,i);
+	}
+}
+int main(void)
+{
+
+// ===========================================================================
+// ============= INIT PERIPH (faites qu'une seule fois)  =====================
+// ===========================================================================
+
+// Après exécution : le coeur CPU est clocké à 72MHz ainsi que tous les timers
+CLOCK_Configure();
+Systick_Period_ff(360000);
+Systick_Prio_IT('0',callback_systick);
+SysTick_On;
+SysTick_Enable_IT;
+
+Init_TimingADC_ActiveADC_ff(ADC1,72);
+Single_Channel_ADC(ADC1,2);
+Init_Conversion_On_Trig_Timer_ff(ADC1,TIM2_CC2,225);
+Init_ADC1_DMA1(0,signal);
+
+	
+
+//============================================================================	
+
+while(1){}
+	
+		
+
+}
+
